606d1e1b4fc3116b289e544e56a5795af98a9c4b
Nicole Law <nicole.law12@gmail.com>
2023-05-25 17:36:28 -0400

    started routes -n

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,4 @@
--- Users table seeds here (Example)
-INSERT INTO Users (username, password, email)
+INSERT INTO users (username, password, email)
 VALUES ('Michael Jackson', 'Abc123$', 'michaeljackson@example.com'),
        ('Angelina Jolie', 'Xyz789$', 'angelinajolie@example.com'),
        ('Pedro Pascal', 'Lmn456$', 'pedropascal@example.com');
