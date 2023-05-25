84cca6e184d6746bd98e2ef5d41095eff8ad472a
Nicole Law <nicole.law12@gmail.com>
2023-05-25 17:43:43 -0400

    edited createQuiz route

--- routes/createQuiz.js
+++ routes/createQuiz.js
@@ -3,18 +3,8 @@ const router  = express.Router();
 
 //Load homepage
 router.get('/', (req, res) => {
-  res.render('index');
-});
-
-
-module.exports = router;
-
-
-const express = require ('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
   res.render('createQuiz');
 });
+
 
 module.exports = router;
