206d82c0f0b1427ec5059e786df8b4681e9a41f3
Asma Jama <asmajama98@gmail.com>
2023-05-26 15:30:10 -0400

    Add async on login route, add cookie-session and body-parser

--- routes/login.js
+++ routes/login.js
@@ -3,25 +3,27 @@ const router = express.Router();
 const { getUserByEmail } = require('../db/queries/users.js');
 
 // load login page
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   return res.render('login');
 });
 
-// //login
-router.post('/login/:id', (req, res) => {
-  const email = req.body.email.trim();
-  const password = req.body.password.trim();
+//login
+router.post('/', async(req, res) => {
+  const email = req.body.email;
+console.log(email, "hi");
+  const password = req.body.password;
   if (!email || !password) {
     return res.status(400).send('Empty field!');
   }
-  const user = getUserByEmail(email);
+  const user = await getUserByEmail(email);
+  console.log(user, "user");
   if (!user) {
     return res.status(400).send('Unauthorized access!');
   }
   if (user) {
   // Set the user ID in both session and cookies
-    req.session.userId = req.params.id;
-    return res.redirect(`/user/${req.params.id}`);
+    req.session.userId = user.id;
+    return res.redirect(`/user/${user.id}`);
   }
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session')
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -16,6 +18,14 @@ app.set('view engine', 'ejs');
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser())
+app.use(
+  cookieSession({
+    name: "user-session",
+    keys: ["quizApp"],
+    maxAge: 5 * 60 * 60 * 1000,
+  })
+);
 app.use(
   '/styles',
   sassMiddleware({
@@ -41,7 +51,7 @@ const quizRoutes = require('./routes/createQuiz')
 // app.use('/api/users', userApiRoutes);
 // app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
-app.use('/login', loginRoutes)
+app.use('/login', loginRoutes);
 app.use('/createQuiz', createQuizRoutes)
 // Note: mount other resources here, using the same pattern above
 
