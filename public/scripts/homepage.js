20de3ffa12f0d6adc2ab6d2cdb18f03e8bae6d2d
Asma Jama <asmajama98@gmail.com>
2023-05-29 16:05:15 -0400

    Add homepage.js

---
+++ public/scripts/homepage.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+
+//create quiz template
+const createQuiz = (quiz) => {
+
+return `
+  <div class="quiz-row">
+      <button class="quiz-item">${quiz.quiz_name}</button>
+  </div>
+  `
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
