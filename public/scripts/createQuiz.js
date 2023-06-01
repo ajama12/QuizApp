05c126fe8b2176759fb3f631e696c2abaf41b75a
Asma Jama <asmajama98@gmail.com>
2023-05-31 22:21:10 -0400

    Add jQuery to createQuiz

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -1,4 +1,68 @@
 $(document).ready(() => {
-  
 
+  //when we submit the form
+  $("#quiz-submission-form").submit(() => {
+    //ajax request save to database and redirect to new quiz
+    const postNewQuiz = () => {
+      console.log("postNewQuiz reached");
+      const quizData = {
+        userId: $("#user-id").data("user-id"),
+        quizName: $("#quiz_title").val().trim(),
+        quizDesc: $("#quiz_description").val().trim(),
+        isPrivate: $("#private").is(":checked"),
+        questions: [
+          {
+            questionText: $("#question_1").val().trim(),
+            answers: [
+              $("#answer1_1").val().trim(),
+              $("#answer1_2").val().trim(),
+            ],
+          },
+          {
+            questionText: $("#question_2").val().trim(),
+            answers: [
+              $("#answer2_1").val().trim(),
+              $("#answer2_2").val().trim(),
+            ],
+          },
+          {
+            questionText: $("#question_3").val().trim(),
+            answers: [
+              $("#answer3_1").val().trim(),
+              $("#answer3_2").val().trim(),
+            ],
+          },
+          {
+            questionText: $("#question_4").val().trim(),
+            answers: [
+              $("#answer4_1").val().trim(),
+              $("#answer4_2").val().trim(),
+            ],
+          },
+          {
+            questionText: $("#question_5").val().trim(),
+            answers: [
+              $("#answer5_1").val().trim(),
+              $("#answer5_2").val().trim(),
+            ],
+          },
+        ],
+      };
+      console.log("quizData:", quizData);
+      const config = {
+        method: "POST",
+        url: "/createQuiz",
+        data: quizData,
+        success: () => {
+          console.log("Successfully created quiz!");
+        },
+        error: (err) => {
+          console.log("Err", err);
+        },
+      };
+
+      $.ajax(config);
+    };
+    postNewQuiz();
+  });
 });
--- routes/createQuiz.js
+++ routes/createQuiz.js
@@ -1,39 +1,50 @@
-const express = require('express');
-const router  = express.Router();
-const { addQuiz } = require('../db/queries/quiz.js');
-const { addQuestion } = require('../db/queries/questions.js');
-const { addAnswer } = require('../db/queries/answers.js');
-
-
+const express = require("express");
+const router = express.Router();
+const { addQuiz } = require("../db/queries/quiz.js");
+const { addQuestion } = require("../db/queries/questions.js");
+const { addAnswer } = require("../db/queries/answers.js");
 
 //load create quiz page
-router.get('/', (req, res) => {
-  res.render('createQuiz');
+router.get("/:user_id", (req, res) => {
+  const user = { id: req.session.userId };
+  const templateVars = { user };
+  res.render("createQuiz", templateVars);
 });
 
 //Create a new quiz
-router.post('/', async(req, res) => {
-  try {
-    const quizResult = await addQuiz(req.body.userId, req.body.quizName, req.body.quizDesc, req.body.isPrivate);
-    const quizId = quizResult.id;
+router.post("/:user_id", async(req, res) => {
+  console.log("create quiz route hit");
+  if (req.session.userId) {
+    try {
+      console.log(req.body);
+      const quizResult = await addQuiz(
+        req.body.userId,
+        req.body.quizName,
+        req.body.quizDesc,
+        req.body.isPrivate
+      );
+      const quizId = quizResult.id;
 
-    for (let question of req.body.questions) {
-      const questionResult = await addQuestion(quizId, question.questionPrompt);
-      const questionId = questionResult.id;
+      for (let question of req.body.questions) {
+        const questionResult = await addQuestion(
+          quizId,
+          question.questionPrompt
+        );
+        const questionId = questionResult.id;
 
-      for (let answer of question.answers) {
-        await addAnswer(quizId, questionId, answer.answer, answer.isCorrect);
+        for (let answer of question.answers) {
+          await addAnswer(quizId, questionId, answer.answer, answer.isCorrect);
+        }
       }
-    }
 
-    res
-      .status(201)
-      .redirect("/quiz/" + quizId);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Something went wrong while creating your quiz.');
+      res.status(201).redirect("/quiz/" + quizId);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Something went wrong while creating your quiz.");
+    }
+  } else {
+    res.status(401).send("Unauthorized! Can't create quiz. Please log in.");
   }
 });
-
 
 module.exports = router;
