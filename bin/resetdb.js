f5ea2e83424d007aae8617ce754c8fba3062690d
Asma Jama <asmajama98@gmail.com>
2023-06-03 02:30:02 -0400

    fix error

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -6,17 +6,20 @@ const fs = require('fs');
 const chalk = require('chalk');
 const db = require('../db/connection');
 
+// PG connection setup
+// const connectionString = process.env.DATABASE_URL ||
+//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+// const client = new Client();
+
 // Loads the schema files from db/schema
 const runSchemaFiles = async () => {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
   const schemaFilenames = fs.readdirSync('./db/schema');
 
   for (const fn of schemaFilenames) {
-    if (fn !== '.DS_Store') {
-      const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
-      console.log(`\t-> Running ${chalk.green(fn)}`);
-      await db.query(sql);
-    }
+    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    console.log(`\t-> Running ${chalk.green(fn)}`);
+    await db.query(sql);
   }
 };
 
@@ -46,4 +49,3 @@ const runResetDB = async () => {
 };
 
 runResetDB();
-
